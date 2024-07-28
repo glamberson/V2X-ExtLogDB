0133c8f41f8473c9caa92bdc47022bbfdf4297a3
Greg Lamberson <lamberson@yahoo.com>
2024-07-28 11:38:48 +0300

    Reset roles and reconfirm vb code approach
    This resets rols to be more permissive for the user roles and restricting the "login" role to the permissions needed. It also should restore the database session management but this need to be confirmed. this is in version 0.7.9.

--- scripts/04 data/001_roles_creation.sql
+++ scripts/04 data/001_roles_creation.sql
@@ -1,40 +1,35 @@
--- version 0.7.5
+-- version 0.7.9
 
 -- create roles and grant pemissions
 
 
 
---Create Login Role
-CREATE ROLE "login" WITH LOGIN PASSWORD 'FOTS-Egypt';
--- Grant USAGE on the schema
-GRANT USAGE ON SCHEMA public TO login;
-GRANT CONNECT ON DATABASE "Beta_003" TO login;
--- Grant EXECUTE permission on the user_login function
-GRANT EXECUTE ON FUNCTION user_login(VARCHAR, VARCHAR, INTERVAL) TO login;
--- If the user_login function needs to access the users table, grant SELECT permission
-GRANT SELECT ON TABLE users TO login;
--- Grant execute permission on the login wrapper function
-GRANT EXECUTE ON FUNCTION login_wrapper(VARCHAR, VARCHAR, INTERVAL) TO login;
+-- Create the "login" role with login capability and NOINHERIT
+CREATE ROLE "login" WITH LOGIN PASSWORD 'FOTS-Egypt' NOINHERIT;
+
+-- Grant SELECT on the users table to validate credentials
+GRANT SELECT ON users TO "login";
+
+-- Grant EXECUTE on necessary functions to the "login" role
+GRANT EXECUTE ON FUNCTION login_wrapper(p_username VARCHAR, p_password VARCHAR, p_duration INTERVAL) TO "login";
+GRANT EXECUTE ON FUNCTION create_session(user_id INT, role_id INT, duration INTERVAL) TO "login";
+GRANT EXECUTE ON FUNCTION log_user_activity(user_id INT, login_time TIMESTAMPTZ, logout_time TIMESTAMPTZ, activity TEXT) TO "login";
+GRANT EXECUTE ON FUNCTION log_failed_login_attempt(username TEXT, reason TEXT) TO "login";
 
 
--- Create KPPO Admin role with full access
-CREATE ROLE kppo_admin_user WITH LOGIN PASSWORD 'admin_password';
-GRANT ALL PRIVILEGES ON DATABASE "Beta_003" TO kppo_admin_user;
+-- Create other roles without login capability and with NOINHERIT
+CREATE ROLE "kppo_admin_user" NOLOGIN NOINHERIT;
+CREATE ROLE "logistics_user" NOLOGIN NOINHERIT;
+CREATE ROLE "report_viewer_user" NOLOGIN NOINHERIT;
 
--- Create logistics role for Chesapeake Warehouse, NAVSUP, and LSC
-CREATE ROLE logistics_user WITH LOGIN PASSWORD 'logistics_password';
-GRANT CONNECT ON DATABASE "Beta_003" TO logistics_user;
-GRANT USAGE ON SCHEMA public TO logistics_user;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO logistics_user;
-GRANT INSERT, UPDATE ON TABLE fulfillment_items TO logistics_user;
-GRANT INSERT, UPDATE ON TABLE line_item_comments TO logistics_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO logistics_user;
+-- Grant database connection privilege
+GRANT CONNECT ON DATABASE "Beta_003" TO "login", "kppo_admin_user", "logistics_user", "report_viewer_user";
 
--- Create role for Report Viewer with specific permissions
-CREATE ROLE report_viewer_user WITH LOGIN PASSWORD 'report_password';
-GRANT CONNECT ON DATABASE "Beta_003" TO report_viewer_user;
-GRANT USAGE ON SCHEMA public TO report_viewer_user;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO report_viewer_user;
-GRANT INSERT, UPDATE ON TABLE line_item_comments TO report_viewer_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO report_viewer_user;
+-- Grant usage on schema
+GRANT USAGE ON SCHEMA public TO "login", "kppo_admin_user", "logistics_user", "report_viewer_user";
+
+-- Grant specific privileges to each role
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "logistics_user";
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "kppo_admin_user";
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "report_viewer_user";
 
