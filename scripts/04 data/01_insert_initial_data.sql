5715836d89e18cce19bec248f5e66bc1e2156db4
Greg Lamberson <lamberson@yahoo.com>
2024-07-28 13:21:42 +0300

    added dummy availability data for Sadeeq
    data v 0.7.9.1

--- scripts/04 data/01_insert_initial_data.sql
+++ scripts/04 data/01_insert_initial_data.sql
@@ -1,3 +1,8 @@
+-- version 0.7.9.1
+
+
+INSERT INTO availability_events (availability_identifier, availability_name, start_date, end_date, description, created_by)
+VALUES ('41', 'Sadeeq', '2024-01-01', '202-01-02', 'This is a dummy description', 1);
 
 
 
