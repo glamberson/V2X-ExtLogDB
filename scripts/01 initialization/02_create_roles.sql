c6fcd49de8a883df8d3e14d2d1affb44b5630735
Greg Lamberson <lamberson@yahoo.com>
2024-07-28 10:21:07 +0300

    Restore permissions and sessions changes.
    Roles table: add db_role_name field and populate data for defined database roles with linked server roles. (file version 0.7.9)

--- scripts/01 initialization/02_create_roles.sql
+++ scripts/01 initialization/02_create_roles.sql
@@ -1,17 +1,18 @@
 -- Create roles table and predefined roles
--- version 0.7
+-- version 0.7.9
 
 CREATE TABLE roles (
     role_id SERIAL PRIMARY KEY, -- Unique identifier for the role
-    role_name VARCHAR(100) UNIQUE NOT NULL -- Name of the role, must be unique
+    role_name VARCHAR(100), UNIQUE NOT NULL -- Name of the role, must be unique
+    db_role_name VARCHAR(100)
 );
 
 
 -- Insert predefined roles
 INSERT INTO roles (role_name) VALUES
-    ('KPPO Admin'),
-    ('Chesapeake Warehouse'),
-    ('NAVSUP'),
-    ('Logistics Service Center (LSC)'),
-    ('Report Viewer');
+    ('KPPO Admin', 'kppo_admin_user'),
+    ('Chesapeake Warehouse', 'logistics_user'),
+    ('NAVSUP', 'logistics_user'),
+    ('Logistics Service Center (LSC)', 'logistics_user'),
+    ('Report Viewer', 'report_ivewer_user');
 
