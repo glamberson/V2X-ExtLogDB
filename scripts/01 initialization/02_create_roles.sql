ab249865fef932d8660979d1f50dc70cc3f6c90d
Greg Lamberson <lamberson@yahoo.com>
2024-07-28 12:23:30 +0300

    fixed syntax errors in roles table creation/insertion. still version 0.7.9

--- scripts/01 initialization/02_create_roles.sql
+++ scripts/01 initialization/02_create_roles.sql
@@ -3,13 +3,13 @@
 
 CREATE TABLE roles (
     role_id SERIAL PRIMARY KEY, -- Unique identifier for the role
-    role_name VARCHAR(100), UNIQUE NOT NULL -- Name of the role, must be unique
+    role_name VARCHAR(100) UNIQUE NOT NULL, -- Name of the role, must be unique
     db_role_name VARCHAR(100)
 );
 
 
 -- Insert predefined roles
-INSERT INTO roles (role_name) VALUES
+INSERT INTO roles (role_name, db_role_name) VALUES
     ('KPPO Admin', 'kppo_admin_user'),
     ('Chesapeake Warehouse', 'logistics_user'),
     ('NAVSUP', 'logistics_user'),
