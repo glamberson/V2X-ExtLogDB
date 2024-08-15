2445b6191a5a5b26917d52d1f1b7c182dfb31a41
Greg Lamberson <lamberson@yahoo.com>
2024-08-15 21:00:54 +0300

    Beta_003 version 0.8.69 wip
    trying to increase batch file sizes without breaking the core that works. these changes are incremental attempts to incread the json size allowance.

--- scripts/04 data/01_insert_initial_data.sql
+++ scripts/04 data/01_insert_initial_data.sql
@@ -1,8 +1,11 @@
--- version 0.8.06
+-- version 0.8.62
 
 
 INSERT INTO availability_events (availability_identifier, availability_name, start_date, end_date, description, created_by)
 VALUES ('41', 'Sadeeq', '2024-01-01', '202-01-02', 'This is a dummy description', 1);
+INSERT INTO availability_events (availability_identifier, availability_name, start_date, end_date, description, created_by)
+VALUES ('66', 'Alexandria', '2024-03-01', '2024-11-11', 'This is a dummy description also', 1);
+
 
 
 
